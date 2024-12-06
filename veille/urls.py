80ce32953cdc9608b958916f6a5258964d6bd4be
julienles879 <julien.lesimple@my-digital-school.org>
2024-12-06 09:49:33 +0100

    ADD user

--- veille/urls.py
+++ veille/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from users import urls as users_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('utilisateur/', include(users_urls))
 ]
