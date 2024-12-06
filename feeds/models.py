7b67277329cbd6100caade5819e3753ab7050e73
julienles879 <julien.lesimple@my-digital-school.org>
2024-12-06 09:54:05 +0100

    ADD Model RSSFeed

--- feeds/models.py
+++ feeds/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class RSSFeed(models.Model):
+    """
+    Modèle pour représenter un flux RSS.
+    """
+    title = models.CharField(max_length=255, help_text="Titre du flux RSS.")
+    url = models.URLField(unique=True, help_text="URL du flux RSS.")
+    description = models.TextField(null=True, blank=True, help_text="Description du flux RSS.")
+    category = models.CharField(max_length=100, null=True, blank=True, help_text="Catégorie associée au flux RSS.")
+    created_at = models.DateTimeField(auto_now_add=True, help_text="Date d'ajout du flux RSS.")
+
+    def __str__(self):
+        return self.title
