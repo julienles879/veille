6b64a35c785a0c26c03e001e9351dda458211494
julienles879 <julien.lesimple@my-digital-school.org>
2025-04-23 22:01:09 +0200

    Refactor Docker setup for improved structure; update Dockerfile to streamline frontend build process and enhance backend configuration. Add gunicorn to requirements and expand ALLOWED_HOSTS for local development.

--- veille/settings.py
+++ veille/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['168.231.106.23', 'julienlesimple.pro']
+ALLOWED_HOSTS = ['168.231.106.23', 'julienlesimple.pro', 'localhost', '127.0.0.1']
 
 
 # Application definition
